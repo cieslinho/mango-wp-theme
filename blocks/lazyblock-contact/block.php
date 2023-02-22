adf6bbb6e1693e13baaaa4ec5e0d88aa15762e4e
Szymon Cieśla <cszymon01@gmail.com>
2023-02-22 02:08:34 +0100

    new: added and styled contact section. fix: residents icons

--- blocks/lazyblock-contact/block.php
+++ blocks/lazyblock-contact/block.php
@@ -1,5 +1,36 @@
 <section id="contact" class="contact">
     <div class="container">
         <h2 class="section-title"><span>kontakt</span></h2>
+        <div class="contact__boxes">
+            <div class="contact__box">
+                <form action="" class="contact__form">
+                    <div class="contact__form-row">
+                        <div class="contact__form-field">
+                            <label for="name" class="contact__label">Imię</label>
+                            <input type="text" id="name" name="name" class="contact__input" required>
+                        </div>
+                        <div class="contact__form-field">
+                            <label for="email" class="contact__label">E-mail</label>
+                            <input type="email" name="email" id="email" class="contact__input" required>
+                        </div>
+                    </div>
+                    <div class="contact__form-row">
+                        <div class="contact__form-field">
+                            <label for="mobile" class="contact__label">Telefon</label>
+                            <input type="tel" name="mobile" id="mobile" class="contact__input" required>
+                        </div>
+                        <div class="contact__form-field">
+                            <label for="subject" class="contact__label">Temat</label>
+                            <input type="subject" name="subject" id="subject" class="contact__input" required>
+                        </div>
+                    </div>
+                   <div class="contact__form-field contact__form-field-textarea">
+                        <label for="msg" name="msg" id="msg" class="contact__label">Wiadomość</label>
+                        <textarea name="msg" id="msg" cols="30" rows="10" class="contact__textarea" required></textarea>
+                        <button type="submit" class="contact__btn">wyślij</button>
+                   </div>
+                </form>
+            </div>
+        </div>
     </div>
 </section>
---
+++ blocks/lazyblock-footer/block.php
@@ -0,0 +1,12 @@
+<!-- <footer class="footer">
+    <div class="container">
+     <div class="footer__boxes">
+        <div class="footer__box footer__box-info">
+            <h2 class="footer__brannd">mango music club</h2>
+            <p class="footer__text">
+                Mango Club Opole to idealne miejsce do spędzania wolnego czasu w każdy weekend! Miejsce, gdzie dobra zabawa i rozrywka serwowane są w najlepszym klubowym wydaniu. 
+            </p>
+        </div>
+     </div>
+    </div>
+</footer> -->
