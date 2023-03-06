db5899b07e9f4873ab8de9ba485aa45c8e6e56ea
Szymon Cieśla <cszymon01@gmail.com>
2023-03-07 00:45:42 +0100

    finished website v5

--- blocks/lazyblock-header/block.php
+++ blocks/lazyblock-header/block.php
@@ -4,11 +4,11 @@
 <header class="header">
     <div class="header__boxes">
         <div class="header__box">
-            <video loop src="<?php echo get_template_directory_uri(); ?>/assets/videos/klub.mp4" class="header__video"></video>
+            <video loop src="<?php echo esc_url( $attributes['club']['url'] ); ?>" class="header__video"></video>
                 <a href="/klub" class="header__link">klub</a>
         </div>
         <div class="header__box">
-            <video loop src="<?php echo get_template_directory_uri(); ?>/assets/videos/scena.mp4" class="header__video"></video>
+            <video loop src="<?php echo esc_url( $attributes['scene']['url'] ); ?>" class="header__video"></video>
             <a href="/koncerty" class="header__link">scena</a>
         </div>
     </div>
